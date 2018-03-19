02a766300ef7c3fe5a9bcac2e340637a00212a22
chenyang <cxy404@hotmail.com>
2018-03-19 17:01:46 +0800

    v0.0.2 进行了功能性代码删除

--- src/main/java/com/ideal/main/SimHash.java
+++ src/main/java/com/ideal/main/SimHash.java
@@ -58,7 +58,7 @@ public class SimHash {
         while ((lexeme = ik.next()) != null) {
             word = lexeme.getLexemeText();
             // 注意停用词会被干掉
-            // System.out.println(word);
+            System.out.println(word);
             // 2、将每一个分词hash为一组固定长度的数列.比如 64bit 的一个整数.
             BigInteger t = this.hash(word);
             for (int i = 0; i < this.hashbits; i++) {
@@ -88,7 +88,7 @@ public class SimHash {
             }
         }
         this.strSimHash = simHashBuffer.toString();
-        System.out.println(this.strSimHash + " length " + this.strSimHash.length());
+        //System.out.println(this.strSimHash + " length " + this.strSimHash.length());
         return fingerprint;
     }
 
@@ -165,7 +165,7 @@ public class SimHash {
             if ((i + 1) % numEach == 0) {
                 // 将二进制转为BigInteger
                 BigInteger eachValue = new BigInteger(buffer.toString(), 2);
-                System.out.println("----" + eachValue);
+                //System.out.println("----" + eachValue);
                 buffer.delete(0, buffer.length());
                 characters.add(eachValue);
             }
@@ -175,27 +175,27 @@ public class SimHash {
     }
 
     /**
-     *  传统的 hash 算法只负责将原始内容尽量均匀随机地映射为一个签名值，"
-     *  "原理上相当于伪随机数产生算法。产生的两个签名，如果相等，说明原始内容在一定概 率 下是相等的；"
-     *  "如果不相等，除了说明原始内容不相等外，不再提供任何信息，因为即使原始内容只相差一个字节，"
-     *  "所产生的签名也很可能差别极大。从这个意义 上来 说，要设计一个 hash 算法，"
-     *  "对相似的内容产生的签名也相近，是更为艰难的任务，因为它的签名值除了提供原始内容是否相等的信息外，"
-     *  "还能额外提供不相等的 原始内容的差异程度的信息。
-     *
-     *  原理上相当于伪随机数产生算法。产生的两个签名，如果相等，说明原始内容在一定概 率 下是相等的；"
-     *  "如果不相等，除了说明原始内容不相等外，不再提供任何信息，因为即使原始内容只相差一个字节，"
-     *  "所产生的签名也很可能差别极大。从这个意义 上来 说，要设计一个 hash 算法，"
-     *  "对相似的内容产生的签名也相近，是更为艰难的任务，因为它的签名值除了提供原始内容是否相等的信息外，"
-     *  "干扰1还能额外提供不相等的 原始内容的差异程度的信息。
-     *
-     *  imhash算法的输入是一个向量，输出是一个 f 位的签名值。为了陈述方便，"
-     *  "假设输入的是一个文档的特征集合，每个特征有一定的权重。"
-     *  "传统干扰4的 hash 算法只负责将原始内容尽量均匀随机地映射为一个签名值，"
-     *  "原理上这次差异有多大呢3相当于伪随机数产生算法。产生的两个签名，如果相等，"
-     *  "说明原始内容在一定概 率 下是相等的；如果不相等，除了说明原始内容不相等外，不再提供任何信息，"
-     *  "因为即使原始内容只相差一个字节，所产生的签名也很可能差别极大。从这个意义 上来 说，"
-     *  "要设计一个 hash 算法，对相似的内容产生的签名也相近，是更为艰难的任务，因为它的签名值除了提供原始"
-     *  "内容是否相等的信息外，干扰1还能额外提供不相等的 原始再来干扰2内容的差异程度的信息。
+     * 传统的 hash 算法只负责将原始内容尽量均匀随机地映射为一个签名值，"
+     * "原理上相当于伪随机数产生算法。产生的两个签名，如果相等，说明原始内容在一定概 率 下是相等的；"
+     * "如果不相等，除了说明原始内容不相等外，不再提供任何信息，因为即使原始内容只相差一个字节，"
+     * "所产生的签名也很可能差别极大。从这个意义 上来 说，要设计一个 hash 算法，"
+     * "对相似的内容产生的签名也相近，是更为艰难的任务，因为它的签名值除了提供原始内容是否相等的信息外，"
+     * "还能额外提供不相等的 原始内容的差异程度的信息。
+     * <p>
+     * 原理上相当于伪随机数产生算法。产生的两个签名，如果相等，说明原始内容在一定概 率 下是相等的；"
+     * "如果不相等，除了说明原始内容不相等外，不再提供任何信息，因为即使原始内容只相差一个字节，"
+     * "所产生的签名也很可能差别极大。从这个意义 上来 说，要设计一个 hash 算法，"
+     * "对相似的内容产生的签名也相近，是更为艰难的任务，因为它的签名值除了提供原始内容是否相等的信息外，"
+     * "干扰1还能额外提供不相等的 原始内容的差异程度的信息。
+     * <p>
+     * imhash算法的输入是一个向量，输出是一个 f 位的签名值。为了陈述方便，"
+     * "假设输入的是一个文档的特征集合，每个特征有一定的权重。"
+     * "传统干扰4的 hash 算法只负责将原始内容尽量均匀随机地映射为一个签名值，"
+     * "原理上这次差异有多大呢3相当于伪随机数产生算法。产生的两个签名，如果相等，"
+     * "说明原始内容在一定概 率 下是相等的；如果不相等，除了说明原始内容不相等外，不再提供任何信息，"
+     * "因为即使原始内容只相差一个字节，所产生的签名也很可能差别极大。从这个意义 上来 说，"
+     * "要设计一个 hash 算法，对相似的内容产生的签名也相近，是更为艰难的任务，因为它的签名值除了提供原始"
+     * "内容是否相等的信息外，干扰1还能额外提供不相等的 原始再来干扰2内容的差异程度的信息。
      *
      * @param args
      * @throws IOException
@@ -208,24 +208,16 @@ public class SimHash {
         // 计算 海明距离 在 3 以内的各块签名的 hash 值
         hash1.subByDistance(hash1, 3);
 
-        // 删除首句话，并加入两个干扰串
-        s = "上海腾讯公司";
+        // 删除首句话，并加入干扰串
+        s = "上海腾讯数码有限公司";
         SimHash hash2 = new SimHash(s, 64);
         System.out.println(hash2.intSimHash + "  " + hash2.intSimHash.bitCount());
         hash1.subByDistance(hash2, 3);
 
-        // 首句前添加一句话，并加入四个干扰串
-        s = "上海腾讯信息科技有限公司";
-        SimHash hash3 = new SimHash(s, 64);
-        System.out.println(hash3.intSimHash + "  " + hash3.intSimHash.bitCount());
-        hash1.subByDistance(hash3, 3);
-
         System.out.println("============================");
 
         int dis = hash1.getDistance(hash1.strSimHash, hash2.strSimHash);
-        System.out.println(hash1.hammingDistance(hash2) + " " + dis);
-        // 根据鸽巢原理（也成抽屉原理，见组合数学），如果两个签名的海明距离在 3 以内，它们必有一块签名subByDistance()完全相同。
-        int dis2 = hash1.getDistance(hash1.strSimHash, hash3.strSimHash);
-        System.out.println(hash1.hammingDistance(hash3) + " " + dis2);
+        System.out.println(hash1.hammingDistance(hash2));
+        //System.out.println(hash1.hammingDistance(hash2) + " " + dis);
     }
 }
